1eb5f7dd2efa77cceab9e84d265b8980b63e7576
Borruso <gborruso@dinamicheaziendali.it>
2023-09-28 14:31:15 +0200

    [UPD] l10n_it_cu: aggiornato migrazione

--- l10n_it_cu/migrations/16.0.1.0.0/post-migrate.py
+++ l10n_it_cu/migrations/16.0.1.0.0/post-migrate.py
@@ -6,17 +6,17 @@ def update_account_cu_statement_type_2022(env):
     acst_2022 = env.ref("l10n_it_cu.account_cu_statement_type_se_2022")
     query_2022 = """
             UPDATE 
-                account_cu_statement acs
+                account_cu_statement
             SET 
-                acs.statement_type_id = %(id_acst_2022)s
+                statement_type_id = %(id_acst_2022)s
             WHERE 
-                acs.statement_type_id IN (
+                statement_type_id IN (
                     SELECT 
                         id 
                     FROM 
                         account_cu_statement_type 
                     WHERE 
-                        name = %(name_acst_2022)s
+                        name = '%(name_acst_2022)s'
                 )
         """ % {
         "id_acst_2022": acst_2022.id,
@@ -31,17 +31,17 @@ def update_account_cu_statement_type_2023(env):
     acst_2023 = env.ref("l10n_it_cu.account_cu_statement_type_se_2023")
     query_2023 = """
         UPDATE 
-            account_cu_statement acs
+            account_cu_statement
         SET 
-            acs.statement_type_id = %(id_acst_2023)s
+            statement_type_id = %(id_acst_2023)s
         WHERE 
-            acs.statement_type_id IN (
+            statement_type_id IN (
                 SELECT 
                     id 
                 FROM 
                     account_cu_statement_type 
                 WHERE 
-                    name = %(name_acst_2023)s
+                    name = '%(name_acst_2023)s'
             )
     """ % {
         "id_acst_2023": acst_2023.id,
